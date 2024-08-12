e233908306a60bd3e94e9bd9e459cec059e00bdd
Shashi preetham Nathi <nathishashipreetham@gmail.com>
2024-08-12 18:23:22 +0530

    Update Create.java

--- Create.java
+++ Create.java
@@ -1,7 +1,20 @@
-public class Create{
-public static void main(String[]args){
-// Create an Object for ArrayList
-  // Load Values in ArrayList
-  // Iterate it by usig Iterator and While Loops
-}
-}
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+public class prac{
+    public static void main(String[]args){
+    // Create an Object for ArrayList
+      // Load Values in ArrayList
+      // Iterate it by usig Iterator and While Loops
+      ArrayList<Integer> s=new ArrayList<>();
+       s.add(3);
+       s.add(002);
+       s.add(0000003);
+       s.add(0607);
+      Iterator i=s.iterator();
+     while(i.hasNext()){
+        System.out.println(i.next());
+     }
+    }
+    }
