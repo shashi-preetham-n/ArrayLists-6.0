cdbd35ab760eb1229c6a6dd5a8190ed199e8a17b
Shashi preetham Nathi <nathishashipreetham@gmail.com>
2024-08-12 18:35:02 +0530

    Update ForL.java

--- ForL.java
+++ ForL.java
@@ -1,7 +1,18 @@
-public class ForL{
-public static void main(String[]args){
-// prepare an object for ArrayList
-  // Load String instead of Integers 
-  // Iterate them using for each loop
-}
-}
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class prac{
+    public static void main(String[]args){
+    // prepare an object for ArrayList
+      // Load String instead of Integers 
+      // Iterate them using for each loop
+       ArrayList<String> s=new ArrayList<>();
+       s.add("shashi");
+       s.add("ravi");
+       s.add("bharath");
+       Iterator i=s.iterator();
+       for(String e:s){
+        System.out.println(s);
+       }
+    }
+    }
